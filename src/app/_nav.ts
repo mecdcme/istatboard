452c1b64e8f258de00909f4196ee2b158734aab9
mecdcme <mecbox.dcme@gmail.com>
2019-01-07 12:13:09 +0100

    Created new repo
    There is still a lot of work to be done!

---
+++ src/app/_nav.ts
@@ -0,0 +1,29 @@
+export const navItems = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Components'
+  },
+  {
+    name: 'Charts',
+    url: '/charts',
+    icon: 'icon-pie-chart'
+  },
+  {
+    name: 'Widgets',
+    url: '/widgets',
+    icon: 'icon-calculator',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  }
+];
---
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnDestroy} from '@angular/core';
+import { navItems } from './../../_nav';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './default-layout.component.html'
+})
+export class DefaultLayoutComponent implements OnDestroy {
+  public navItems = navItems;
+  public sidebarMinimized = true;
+  private changes: MutationObserver;
+  public element: HTMLElement = document.body;
+  constructor() {
+
+    this.changes = new MutationObserver((mutations) => {
+      this.sidebarMinimized = document.body.classList.contains('sidebar-minimized');
+    });
+
+    this.changes.observe(<Element>this.element, {
+      attributes: true,
+      attributeFilter: [ 'class' ]
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.changes.disconnect();
+  }
+}
