b9e25992d626a61a2456db25b6f8c1d30817e5d9
Francesco Amato <framato@pc.istat.it>
2019-02-04 15:21:35 +0100

    ng2table

--- src/app/views/istat-maps/istat-maps.component.ts
+++ src/app/views/istat-maps/istat-maps.component.ts
@@ -42,24 +42,38 @@ export class IstatMapsComponent implements OnInit {
   @ViewChild('popup') popup: ElementRef;
   @ViewChild('popupContent') popupContent: ElementRef;
 
+  // ng-table
   public rowsFeature:Array<any> = [];
+  public rows:Array<any> = [];
   public columnsFeature:Array<any> = [
     {title: 'ID', name: 'id', sort:true, filtering: {filterString: '', placeholder: 'Filter by Id'} },
     {title: 'Terrain type', name: 'name',  sort: true, filtering: {filterString: '', placeholder: 'Filter by type'} },
     {title: 'Center Map', name: 'center'  },
   ];
 
-
-
+  public page:number = 1;
+  public itemsPerPage:number = 10;
+  public maxSize:number = 5;
+  public numPages:number = 1;
+  public length:number = 0;
+  public totalItems:number = 0;
+  public config:any = {
+    paging: true,
+    sorting: {columns: this.columnsFeature},
+    filtering: {filterString: ''},
+    className: ['table-striped', 'table-bordered']
+  };
+  private data:Array<any> = this.rowsFeature;
 
   constructor() {
     this.gridVisible =true;
+    this.length = this.data.length;
   }
 
   ngOnInit() {
     let _thisC = this;
     this.popup.nativeElement.style.display = "none";
-
+    this.onChangeTable(this.config);
 
     /**
            * Create an overlay to anchor the popup to the map.
@@ -118,38 +132,16 @@ export class IstatMapsComponent implements OnInit {
     this.map.on('pointermove', function (evt) {
       
       let features =  this.getFeaturesAtPixel(evt.pixel) ;
-      console.log(features); 
+      let coordinate = evt.coordinate;
+       
       var feature = this.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
-      
-        return feature;
+         return feature;
       });
       
       if (feature) {
        
-        //   document.getElementById('popup').style.display = "inline";
-        _thisC.popup.nativeElement.style.display = "inline";
-
-        let coordinate = evt.coordinate;
-
-        _thisC.popupContent.nativeElement.innerHTML = feature.get('description');
-
-        let overlay;
-        if (this.getOverlays().getLength() > 0) {
-          overlay = this.getOverlays().getArray()[0];
-          overlay.setPosition(coordinate);
+        _thisC.viewPopupFeature(feature,coordinate);
         }
-        else {
-          overlay = new Overlay({
-            element: _thisC.popup.nativeElement,//document.getElementById('popup'),
-
-            position: coordinate,
-
-          });
-          this.addOverlay(overlay);
-        }
-
-
-      }
       else {
         _thisC.popup.nativeElement.style.display = "none";
 
@@ -170,11 +162,37 @@ export class IstatMapsComponent implements OnInit {
           _thisC.rowsFeature.push({id:feat.getId(),name:feat.get('name'),center:_thisC.button(feat.getId())});
        }
        
-    
-       
+       _thisC.length = _thisC.rowsFeature.length;
+       _thisC.data=_thisC.rowsFeature;
+       _thisC.onChangeTable( _thisC.config);
+     // _thisC.page=1;
+      // _thisC.changePage( _thisC.page)  ;
       }
     });
 
+  }
+  viewPopupFeature(feature: any,coordinate:any): any {
+     //   document.getElementById('popup').style.display = "inline";
+     this.popup.nativeElement.style.display = "inline";
+
+   
+     this.popupContent.nativeElement.innerHTML = feature.get('description');
+
+     let overlay;
+     if (this.map.getOverlays().getLength() > 0) {
+       overlay = this.map.getOverlays().getArray()[0];
+       overlay.setPosition(coordinate);
+     }
+     else {
+       overlay = new Overlay({
+         element:this.popup.nativeElement,//document.getElementById('popup'),
+
+         position: coordinate,
+
+       });
+       this.map.addOverlay(overlay);
+     }
+    
   }
   over(evt) {
     var coordinate = evt.coordinate;
@@ -199,20 +217,109 @@ export class IstatMapsComponent implements OnInit {
     this.view.fit(polygon, { padding: [170, 50, 30, 150], constrainResolution: false });
  
   }
-  public centerPolygonId() { 
-   alert('a');
-    //idfeat: string='1';
-    //var feat= this.vectorLayerKlm.getSource().getFeatureById(idfeat);
-   // var polygon = feat.getGeometry();
-   // this.view.fit(polygon, { padding: [170, 50, 30, 150], constrainResolution: false });
+  public centerPolygonId(event) {
+    let idfeat: string = event.row.id;
+    let feat= this.vectorLayerKlm.getSource().getFeatureById(idfeat);
+    let polygon = feat.getGeometry();
+    var center =  feat.getGeometry().getCoordinates()[0];
+    this.viewPopupFeature(feat,center)
+     this.view.fit(polygon, { padding: [170, 50, 30, 150] });
+     var size = /** @type {module:ol/size~Size} */ (this.map.getSize());
+     this.view.centerOn(polygon.getCoordinates());
  
   }
 
  
-  @Output() public button(idfeat: string) {
-       return '<button class="btn btn-sm btn-primary" onClick="centerPolygonId()"><i class="fa fa-map-marker"></i></button>';
+ public button(idfeat: string) {
+       return '<button class="btn btn-sm btn-primary" ><i class="fa fa-map-marker"></i></button>';
     // return '<button class="btn btn-sm btn-primary" (click)="centerPolygonId(\''+ idfeat + '\')"><i class="fa fa-map-marker"></i></button>';
 }
+public changePage(page:any, data:Array<any> = this.data):Array<any> {
+  let start = (page.page - 1) * page.itemsPerPage;
+  let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : data.length;
+  return data.slice(start, end);
+}
 
+public changeSort(data:any, config:any):any {
+  if (!config.sorting) {
+    return data;
+  }
+
+  let columns = this.config.sorting.columns || [];
+  let columnName:string = void 0;
+  let sort:string = void 0;
+
+  for (let i = 0; i < columns.length; i++) {
+    if (columns[i].sort !== '' && columns[i].sort !== false) {
+      columnName = columns[i].name;
+      sort = columns[i].sort;
+    }
+  }
+
+  if (!columnName) {
+    return data;
+  }
+
+  // simple sorting
+  return data.sort((previous:any, current:any) => {
+    if (previous[columnName] > current[columnName]) {
+      return sort === 'desc' ? -1 : 1;
+    } else if (previous[columnName] < current[columnName]) {
+      return sort === 'asc' ? -1 : 1;
+    }
+    return 0;
+  });
+}
+
+public changeFilter(data:any, config:any):any {
+  let filteredData:Array<any> = data;
+  this.columnsFeature.forEach((column:any) => {
+    if (column.filtering) {
+      filteredData = filteredData.filter((item:any) => {
+        return item[column.name].match(column.filtering.filterString);
+      });
+    }
+  });
+
+  if (!config.filtering) {
+    return filteredData;
+  }
+
+  if (config.filtering.columnName) {
+    return filteredData.filter((item:any) =>
+      item[config.filtering.columnName].match(this.config.filtering.filterString));
+  }
+
+  let tempArray:Array<any> = [];
+  filteredData.forEach((item:any) => {
+    let flag = false;
+    this.columnsFeature.forEach((column:any) => {
+      if (item[column.name].toString().match(this.config.filtering.filterString)) {
+        flag = true;
+      }
+    });
+    if (flag) {
+      tempArray.push(item);
+    }
+  });
+  filteredData = tempArray;
+
+  return filteredData;
+}
+
+public onChangeTable(config:any, page:any = {page: this.page, itemsPerPage: this.itemsPerPage}):any {
+  if (config.filtering) {
+    Object.assign(this.config.filtering, config.filtering);
+  }
+
+  if (config.sorting) {
+    Object.assign(this.config.sorting, config.sorting);
+  }
+
+  let filteredData = this.changeFilter(this.rowsFeature, this.config);
+  let sortedData = this.changeSort(filteredData, this.config);
+  this.rows = page && config.paging ? this.changePage(page, sortedData) : sortedData;
+  this.length = sortedData.length;
+}
 
 }
